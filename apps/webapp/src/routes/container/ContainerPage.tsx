b73a3da0ae00dbf17359718ad6f0ee3fa0cd6789
Ben Hacker <Sigabiel123@gmail.com>
2025-12-12 15:19:20 +0100

    chore: remove unused imports

--- apps/webapp/src/routes/container/ContainerPage.tsx
+++ apps/webapp/src/routes/container/ContainerPage.tsx
@@ -1,22 +1,7 @@
-import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import { useSession } from '@/context/SessionContext';
 import { ContainerList } from '@/routes/container/ContainerList';
-import { CreateContainerDialog } from '@/routes/container/CreateContainerDialog';
-import { DeleteContainerDialog } from '@/routes/container/DeleteContainer';
-import { EditContainerDialog } from '@/routes/container/EditContainerDialog';
-import type { Container } from '@sigauth/generics/prisma-types';
-import { PROTECTED } from '@sigauth/generics/protected';
-import { Edit, Trash } from 'lucide-react';
-import { useState } from 'react';
 
 export const ContainerPage: React.FC = () => {
-    const { session } = useSession();
-
-    const [editContainer, setEditContainer] = useState<Container | undefined>(undefined);
-    const [deleteContainer, setDeleteContainer] = useState<Container | undefined>(undefined);
-
     return (
         <>
             <h2 className="scroll-m-20 text-3xl font-semibold">Manage Containers</h2>
