2c49d6ad8291a4dfec1690dcb8149b1d2afabd14
Ben Hacker <Sigabiel123@gmail.com>
2025-12-13 19:46:13 +0100

    feat: add logger to main.ts and delete unused env template

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,12 @@
 import { UnauthorizedExceptionFilter } from '@/common/filters/unauthorized-exception.filter';
-import { RequestMethod, ValidationPipe } from '@nestjs/common';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+    const logger = new Logger('Main');
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api', {
         exclude: [{ path: '.well-known/*path', method: RequestMethod.GET }],
@@ -17,15 +18,15 @@ async function bootstrap() {
     if (process.env.EXPOSE_SWAGGER === 'true') {
         const config = new DocumentBuilder()
             .setTitle('SigAuth API')
-            .setDescription('The SigAuth API is rate limited and protected by 2FA. You can\'t send more than 10 requests per minute.')
+            .setDescription("The SigAuth API is rate limited and protected by 2FA. You can't send more than 10 requests per minute.")
             .setVersion('0.2')
             .build();
 
         const document = SwaggerModule.createDocument(app, config);
         SwaggerModule.setup('api/docs', app, document);
-        console.log('SigAuth Swagger is exposed at /api/docs');
+        logger.log('SigAuth Swagger Documentation exposed at /api/docs');
     } else {
-        console.log('SigAuth Swagger is disabled by environment variable');
+        logger.log('SigAuth Swagger Documentation is disabled by environment variable');
     }
 
     await app.listen(process.env.PORT ?? 4000);
