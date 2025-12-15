541cf21d34fe5513a766b1109b4ad242611db63c
Ben Hacker <Sigabiel123@gmail.com>
2025-12-15 07:43:04 +0100

    feat: add cronjob for webfetches

---
+++ apps/api/src/modules/app/app.cron.ts
@@ -0,0 +1,41 @@
+import { PrismaService } from '@/common/prisma/prisma.service';
+import { AppsService } from '@/modules/app/app.service';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { AppPermission } from '@sigauth/generics/json-types';
+
+@Injectable()
+export class AppWebFetchCron {
+    private readonly logger: Logger = new Logger(AppWebFetchCron.name);
+
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly appService: AppsService,
+    ) {
+        this.tick();
+    }
+
+    @Cron('0 0 * * *') // Every day at midnights
+    async tick() {
+        const apps = await this.prisma.app.findMany({ where: { webFetch: { path: ['enabled'], equals: true } } });
+        for (const app of apps) {
+            try {
+                const permissions = await this.appService.fetchPermissionsFromURL(app.url);
+                if (permissions) {
+                    // look for duplicate identifiers in permissions
+                    const allPerms = Object.values(permissions).flat();
+                    const uniquePerms = Array.from(new Set(allPerms));
+                    if (allPerms.length !== uniquePerms.length) {
+                        this.logger.warn(`Duplicate permissions found for app ${app.name} (${app.id}). Skipping update.`);
+                        continue;
+                    }
+
+                    await this.appService.clearDeletedPermissions(app.id, app.permissions as AppPermission, permissions);
+                    this.logger.log(`Updated permissions for app ${app.name} (${app.id}) from web fetch.`);
+                }
+            } catch (error: any) {
+                this.logger.error(`Failed to fetch permissions for app ${app.name} (${app.id}): ${error.message}`);
+            }
+        }
+    }
+}
--- apps/api/src/modules/app/app.module.ts
+++ apps/api/src/modules/app/app.module.ts
@@ -1,14 +1,15 @@
-import { Module } from '@nestjs/common';
 import { PrismaService } from '@/common/prisma/prisma.service';
-import { HttpModule } from '@nestjs/axios';
 import { AppsController } from '@/modules/app/app.controller';
+import { AppWebFetchCron } from '@/modules/app/app.cron';
 import { AppsService } from '@/modules/app/app.service';
 import { AuthGuard } from '@/modules/auth/guards/authentication.guard';
 import { IsRoot } from '@/modules/auth/guards/authentication.is-root.guard';
+import { HttpModule } from '@nestjs/axios';
+import { Module } from '@nestjs/common';
 
 @Module({
     imports: [HttpModule],
     controllers: [AppsController],
-    providers: [AppsService, AuthGuard, IsRoot, PrismaService],
+    providers: [AppsService, AuthGuard, IsRoot, PrismaService, AppWebFetchCron],
 })
 export class AppsModule {}
